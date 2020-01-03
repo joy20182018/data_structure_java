d890a51acac298fec08f53d0eabf5167edb45e38
joy20182018 <joytribianni2000@gmail.com>
2020-01-03 09:50:24 +0800

    add AVLTree.c

---
+++ AVLTree/AVLTree_c/cmake-build-debug/CMakeFiles/AVLTree_im.h.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AVLTree_im.h.exe"
+  "AVLTree_im.h.exe.manifest"
+  "AVLTree_im.h.pdb"
+  "CMakeFiles/AVLTree_im.h.dir/main.c.obj"
+  "libAVLTree_im.h.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVLTree_im.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AVLTree/AVLTree_c/cmake-build-debug/CMakeFiles/AVLTree_im.h.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AVLTree_im.h.
+# This may be replaced when dependencies are built.
